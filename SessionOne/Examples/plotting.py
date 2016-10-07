49c9f5457bd5d052d3d24dda0e6b7ba6b147aa4f
Thomas Boudreaux <thomas@boudreauxmail.com>
2016-10-07 08:49:41 -0400

    Major additions to ploting

--- SessionOne/Examples/plotting.py
+++ SessionOne/Examples/plotting.py
@@ -18,10 +18,40 @@ Matplotlib section
 
 
 def update_plot(event):     # takes slider event (think about like namespace)
-    global ax, a, Spec      # lazy code, but here to allow easy interaction with other objects
-    a.set_ydata(Spec['amp'] * (ApInc.val/500))  # change the data in y based on the current slider value
-    # ax.set_ylim([0, max(Spec['amp']*ApInc.val/500)])  # change the scaling on the y axis
-    plt.draw()  # redraw the plot
+    global ax, a, Spec, data_state      # lazy code, but here to allow easy interaction with other objects
+    if data_state is True:
+        a.set_ydata(Spec['amp'] * ApInc.val)  # change the data in y based on the current slider value
+        # ax.set_ylim([0, max(Spec['amp']*ApInc.val)])  # change the scaling on the y axis
+        plt.draw()  # redraw the plot
+    else:
+        pass
+
+
+def toggle_data(event):
+    global ax, a, Spec, data_state
+    if data_state is True:
+        a.set_ydata([])
+        a.set_xdata([])
+        plt.draw()
+        data_state = False
+    else:
+        a.set_ydata(Spec['amp'] * ApInc.val)
+        a.set_xdata(Spec['freq'])
+        plt.draw()
+        data_state = True
+
+
+def onclick(event):
+    global ax, a, Spec, data_state
+    print 'Position:', event.x, event.y
+    if data_state is True:
+        delta_nearest_data = (min([abs(event.xdata-i) for i in Spec['freq']]),
+                              min([abs(event.ydata-i) for i in Spec['amp']*ApInc.val]))
+        print delta_nearest_data
+        nearest_data = (delta_nearest_data[0] + event.xdata, delta_nearest_data[1] + event.ydata)
+        print nearest_data
+        ax.plot([nearest_data[0]], [nearest_data[1]], 'D')
+        plt.draw()
 
 spectrum1_name = '../Data/testspectrum.tsv'     # Path name to data
 
@@ -39,12 +69,22 @@ ax.grid()       # toggle grid lines on
 ax.axvline(x=8200, linestyle='-.', color='r')       # add a vertical line at specified x
 ax.axhline(y=1.5*10**-15, linestyle='--', color='g')        # Add a horizontal line at specified y
 
+                # xpos  ypos xwidth ywidth
 axApp = plt.axes([0.25, 0.01, 0.65, 0.01])      # define a new set of axes (some box to do stuff on)
-ApInc = plt.Slider(axApp, 'Scaling', 0, 1000, valinit=500)  # slave a slider to those axis -- between 0, 1000 start 500
+ApInc = plt.Slider(axApp, 'Scaling', 0, 2, valinit=1, dragging=True)  # slave a slider to those axis -- between 0, 2 start 1
 ApInc.on_changed(update_plot)   # when slider changes call update_plot function
+
+data_state = True
+
+axButton = plt.axes([0.915, 0.5, 0.075, 0.1])      # define a new set of axes (some box to do stuff on)
+bToggle = plt.Button(axButton, 'Toggle')  # slave a slider to those axis -- between 0, 2 start 1
+bToggle.on_clicked(toggle_data)   # when slider changes call update_plot function
 
 
 ax.set_xlabel('Frequency (' + u'\u00B5' + 'Hz)')    # x label including unicode character
 ax.set_ylabel('Amplitude')
 ax.set_title('FT of some GALEX target')
+
+cid = fig.canvas.mpl_connect('button_press_event', onclick)
+
 plt.show()  # Display graph object
